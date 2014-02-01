9c4ea57feff4b0f54938319070982b18962e7f22
Davy <davy@Davy-Portable.home>
2014-02-01 15:23:18 +0100

    sfArkLib take as input a QDataStream
    Former-commit-id: 1bcdb309c520c679587e5f0eb252f64062589a4b [formerly 5278cde13f6c79050cc2f8f8133c5dedcb7008c7]
    Former-commit-id: 2dd7c39a8e4fc0445f621ddad757c9c65478a826
    Former-commit-id: f1e4044e49ed20fdd633a849ee11928ca0c632fd [formerly 45f5ac7c8ad6b3c134adc4d1a64c07c575711909]
    Former-commit-id: 79795b3d9f0765d74e67923a9cb32984da34b91e

--- trunk/sfark/sfklFile.cpp
+++ trunk/sfark/sfklFile.cpp
@@ -25,7 +25,6 @@
 #include <string.h>
 
 #include "wcc.h"
-//#include "zlib.h"	// only needed for debug printf
 
 #ifdef TARGET_WIN32
 // Windows32 target
@@ -38,46 +37,27 @@
 #define NOT_OVERLAPPED			((LPOVERLAPPED) 0)
 #define	NO_TEMPLATE			((HANDLE) 0)
 
-#define	CREATEFILE(FileName) 	CreateFile((LPCTSTR)FileName, GENERIC_WRITE, FILE_SHARE_NONE, NO_SECURITY, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL + FILE_FLAG_SEQUENTIAL_SCAN, NO_TEMPLATE)
-#define OPENFILE(FileName)	CreateFile((LPCTSTR)FileName, GENERIC_READ,  FILE_SHARE_READ, NO_SECURITY, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL + FILE_FLAG_SEQUENTIAL_SCAN, NO_TEMPLATE)
-#define READFILE(fh, Buf, BytesToRead, pBytesRead)	ReadFile(fh, Buf, BytesToRead, pBytesRead, NOT_OVERLAPPED)
-#define WRITEFILE(fh, Buf, BytesToWrite, pBytesWritten)	ReadFile(fh, Buf, BytesToWrite, pBytesWritten, NOT_OVERLAPPED)
-#define	SETFILEPOINTER(fh, NewPos)	(SetFilePointer(fh, NewPos, 0, FILE_BEGIN))
-#define CLOSEFILE(fh)		(CloseHandle(fh))
 #define GETLASTERROR()		(GetLastError())
 
 // Mac, Linux target
 #else
-typedef FILE* HANDLE;
 
 #define INVALID_HANDLE_VALUE	(NULL)
-
-#define	CREATEFILE(filename) 	fopen(filename, "wb")
-#define OPENFILE(filename)	fopen(filename, "rb")
-#define READFILE(fh, Buf, BytesToRead, pBytesRead)		*pBytesRead = fread(Buf, 1, BytesToRead, fh)
-#define WRITEFILE(fh, Buf, BytesToWrite, pBytesWritten)	*pBytesWritten = fwrite(Buf, 1, BytesToWrite, fh)
-#define SETFILEPOINTER(fh, NewPos)	(fseek(fh, NewPos, SEEK_SET))
-#define CLOSEFILE(fh)		(fclose(fh) == 0)
 #define GETLASTERROR()		(-1)
 #endif
 
 
 // Static data to track current input and output file...
-HANDLE	InputFileHandle = INVALID_HANDLE_VALUE;		// current input file handle
+QDataStream	* InputFileHandle = NULL;	// current input file handle
 QDataStream	* OutputFileHandle = NULL;	// ... output file handle
-char	InFileName[SFARKLIB_MAX_FILEPATH];							// current input file name
 
 // Local prototypes...
 int	ChkErr(const char *message, const char *filename);
 
-void OpenInputFile(const char *FileName)
+void setInputFileHandle(QDataStream * stream)
 {
-    strncpy(InFileName, FileName, sizeof(InFileName));
-    InputFileHandle = OPENFILE(FileName);
-    if (InputFileHandle == INVALID_HANDLE_VALUE)
-        ChkErr("open", InFileName);
+    InputFileHandle = stream;
 }
-
 void setOutputFileHandle(QDataStream * stream)
 {
     OutputFileHandle = stream;
@@ -87,45 +67,23 @@ void setOutputFileHandle(QDataStream * stream)
 
 int ReadInputFile(BYTE *Buf, int BytesToRead)
 {
-    int	BytesRead;
-
-    READFILE(InputFileHandle, Buf, BytesToRead, &BytesRead);
-    if (BytesRead < 0)
-    {
-        ChkErr("read from", InFileName);
-        BytesRead = 0;
-    }
-    return BytesRead;
+    return InputFileHandle->readRawData((char *)Buf, BytesToRead);
 }
 // =================================================================================
 
 int WriteOutputFile(const BYTE *Buf, int BytesToWrite)
 {
-    OutputFileHandle->writeRawData((char *)Buf, BytesToWrite);
-    return BytesToWrite;
+    return OutputFileHandle->writeRawData((char *)Buf, BytesToWrite);
 }
 
 // =================================================================================
 
 bool SetInputFilePosition(int NewPos)
 {
-    if (SETFILEPOINTER(InputFileHandle, NewPos) != 0)
-    {
-        ChkErr("SetInputFilePosition", InFileName);
-        return false;
-    }
-    //else printf("SetInputFilePosition Ok: Requested %d Actual %ld\n", NewPos, ftell(InputFileHandle));
-    return true;
+    QIODevice * device = InputFileHandle->device();
+    return device->seek(NewPos);
 }
-// =================================================================================
 
-void CloseInputFile(void)
-{
-    if (InputFileHandle != INVALID_HANDLE_VALUE && CLOSEFILE(InputFileHandle) == 0)
-        ChkErr("Close input file: ", InFileName);
-    InputFileHandle = INVALID_HANDLE_VALUE;
-    return;
-}
 // =================================================================================
 
 int ChkErr(const char *ErrorMsg, const char *FileName)
