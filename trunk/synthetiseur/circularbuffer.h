e52f0e8d3b4ed2df663b7f8fc348b3d09d0e2b79
Davy <davy@debian-laptop>
2014-10-03 14:59:17 +0200

    Fixed copyright owners
    Andrea Celani added
    
    
    Former-commit-id: 0379a5e47b41c321164194e82e8a7dca1cab7794 [formerly 0958df03a1141eb73a55047de5b81234cf3f5da2]
    Former-commit-id: 673a7850f026ebe08f9390a926dc9fc09bf9d7c5
    Former-commit-id: 68edf9f2b48f563d977540dbc7c4994139598160 [formerly d36f31c68ca6300e7586ec496d0d247b751120de]
    Former-commit-id: 21a385f99dab397fbe967c1c80e91b44c0ce873c

--- trunk/synthetiseur/circularbuffer.h
+++ trunk/synthetiseur/circularbuffer.h
@@ -2,6 +2,7 @@
 **                                                                        **
 **  Polyphone, a soundfont editor                                         **
 **  Copyright (C) 2013-2014 Davy Triponney                                **
+**                2014      Andrea Celani                                 **
 **                                                                        **
 **  This program is free software: you can redistribute it and/or modify  **
 **  it under the terms of the GNU General Public License as published by  **
