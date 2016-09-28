878300b0b5e080d0a6b26c8d506d5485e47e2cc2
davy <davy@debian>
2016-09-28 19:44:28 +0200

    Attenuation tool improvement
    Can now deal with the global divisions
    Change the attenuation at the instrument and / or preset level(s)
    
    
    Former-commit-id: fdf0644129f5c2d06a36cba38b6b4a2cb79effe9 [formerly 015a269c880ee60c337d129acd0b176379ab6688]
    Former-commit-id: 03b7fd85c9988f7a8d24dd82a624be06de1b9a69
    Former-commit-id: 0a3f28b71bd0dba79b9f3c196209be1a53763ef9 [formerly 22f5c5dc99c51d504a8ee07d0b04d4fd073fd7ad]
    Former-commit-id: 82310f1f8947a897ac156c88cccfa14638795070

---
+++ trunk/tools/dialog_attenuation.h
@@ -0,0 +1,53 @@
+/***************************************************************************
+**                                                                        **
+**  Polyphone, a soundfont editor                                         **
+**  Copyright (C) 2013-2015 Davy Triponney                                **
+**                                                                        **
+**  This program is free software: you can redistribute it and/or modify  **
+**  it under the terms of the GNU General Public License as published by  **
+**  the Free Software Foundation, either version 3 of the License, or     **
+**  (at your option) any later version.                                   **
+**                                                                        **
+**  This program is distributed in the hope that it will be useful,       **
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of        **
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         **
+**  GNU General Public License for more details.                          **
+**                                                                        **
+**  You should have received a copy of the GNU General Public License     **
+**  along with this program.  If not, see http://www.gnu.org/licenses/.   **
+**                                                                        **
+****************************************************************************
+**           Author: Davy Triponney                                       **
+**  Website/Contact: http://polyphone-soundfonts.com                      **
+**             Date: 01.01.2013                                           **
+***************************************************************************/
+
+#ifndef DIALOG_ATTENUATION_H
+#define DIALOG_ATTENUATION_H
+
+#include <QDialog>
+
+namespace Ui {
+class DialogAttenuation;
+}
+
+class DialogAttenuation : public QDialog
+{
+    Q_OBJECT
+
+public:
+    DialogAttenuation(double minInst, double maxInst, double minPrst, double maxPrst, QWidget *parent = 0);
+    ~DialogAttenuation();
+
+signals:
+    void accepted(double offsetInst, double offsetPrst);
+
+private slots:
+    void accept();
+
+private:
+    Ui::DialogAttenuation *ui;
+    static QString formatDouble(double value);
+};
+
+#endif // DIALOG_ATTENUATION_H
