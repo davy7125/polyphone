928bd4a89c26337b5e738d5d3f49e943f25f63f7
Davy <davy.triponney@gmail.com>
2018-11-22 16:02:22 +0100

    Application style

--- sources/mainwindow/coloredtabwidget.h
+++ sources/mainwindow/coloredtabwidget.h
@@ -3,6 +3,7 @@
 
 #include <QTabWidget>
 #include <QMap>
+class QPushButton;
 
 class ColoredTabInfo
 {
@@ -10,6 +11,7 @@ public:
     QColor _backgroundColor;
     QColor _textColor;
     QString _iconName;
+    QPushButton * _closeButton;
 };
 
 
@@ -18,11 +20,14 @@ class ColoredTabWidget : public QTabWidget
     Q_OBJECT
 
 public:
-    ColoredTabWidget(QWidget *parent = NULL);
+    static const QSize TAB_ICON_SIZE;
+
+    ColoredTabWidget(QWidget *parent = nullptr);
     int addColoredTab(QWidget *widget, QString iconName, const QString &label, QColor backgroundColor, QColor textColor);
 
 private slots:
     void onCurrentChanged(int index);
+    void onCloseButtonClicked();
 
 private:
     void changeStyleSheet(QColor backgroundColor, QColor textColor);
