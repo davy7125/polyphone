38b4c7edcafe562a9b8e2a903be07ea602adaa85
Bart Massey <bart.massey@gmail.com>
2021-03-12 15:12:46 -0800

    updated sf3 (sftools) source license to LGPLv2 after MuseScore upstream change
    MuseScore's sftools (http://github.com/MuseScore/sftools)
    was the base for the source code in lib/sf3. This code was
    formerly licensed under GPLv2-only, which is incompatible
    with polyphone's GPLv3 license. MuseScore has now updated
    the license for this code to LGPLv2, which allows the
    corresponding code in polyphone to also be placed under this
    license.

--- sources/lib/sf3/sfont.cpp
+++ sources/lib/sf3/sfont.cpp
@@ -5,17 +5,23 @@
 //
 //  Copyright (C) 2010 Werner Schweer and others
 //
-//  This program is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License version 2.
+//  This work is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Library General Public
+//  License as published by the Free Software Foundation; either
+//  version 2 of the License, or (at your option) any later version.
 //
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
+//  In addition, as a special exception, licensor gives permission to
+//  link the code of this work with the OpenSSL Library (or with modified
+//  versions of OpenSSL that use the same license as OpenSSL), and
+//  distribute linked combinations including the two. You must obey the
+//  GNU General Public License in all respects for all of the code used
+//  other than OpenSSL. If you modify this file, you may extend this
+//  exception to your version of the file, but you are not obligated to
+//  do so. If you do not wish to do so, delete this exception statement
+//  from your version. (This exception is necessary should this work be
+//  included in a GPL-licenced work.)
 //
-//  You should have received a copy of the GNU General Public License
-//  along with this program; if not, write to the Free Software
-//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//  See COPYING.LIB for the licence text and disclaimer of warranty.
 //=============================================================================
 
 #include <stdio.h>
