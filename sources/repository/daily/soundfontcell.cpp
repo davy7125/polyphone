512b80ea3c406ccc4fd65fddcac9b91aa84c45fa
Davy <davy.triponney@gmail.com>
2022-01-26 13:03:25 +0100

    Soundfont cell

--- sources/repository/daily/soundfontcell.cpp
+++ sources/repository/daily/soundfontcell.cpp
@@ -126,12 +126,10 @@ void SoundfontCell::on_labelAuthor_linkActivated(const QString &link)
 
 int SoundfontCell::heightForWidth(int width) const
 {
-    // 2 * 9px (margins) + 2 * 6px (spaces) => 30px
-    float coef = 1.0;
-#ifdef Q_OS_MAC
-    coef = 1.5; // Don't know why
-#endif
-    return 30 * coef + ui->line1->height() + ui->line2->height() + ui->line3->heightForWidth(width - 18 * coef);
+    int margin = ui->verticalLayout->contentsMargins().bottom() * 2;
+    int spacing = ui->verticalLayout->spacing() * 2;
+    return margin + spacing + ui->line1->height() + ui->line2->height() +
+            ui->line3->heightForWidth(width - margin);
 }
 
 bool SoundfontCell::hasHeightForWidth() const
